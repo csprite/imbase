83f8b32da36c4f6e85506a5f3e3881cefefe19cf
pegvin <75035219+pegvin@users.noreply.github.com>
2023-08-27 01:02:52 +0530

    Fix Texture resolution not being set on creation

--- src/texture.cpp
+++ src/texture.cpp
@@ -1,13 +1,16 @@
 #include "imbase/texture.hpp"
 #include "glad/glad.h"
 
-ImBase::Texture::Texture(int width, int height, unsigned char* pixels) {
+ImBase::Texture::Texture(int w, int h, unsigned char* pixels) {
 	glGenTextures(1, &id);
 	glBindTexture(GL_TEXTURE_2D, id);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	width = w;
+	height = h;
 
 	unsigned char* data = pixels;
 
