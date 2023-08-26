1001c1a5273cd2e4a2e2737fb2f0aec1ddf37199
pegvin <75035219+pegvin@users.noreply.github.com>
2023-08-26 20:19:57 +0530

    Complete Initialization And Rendering Using OpenGL3

--- src/base/include/imbase/window.hpp
+++ src/base/include/imbase/window.hpp
@@ -6,6 +6,10 @@ namespace ImBase {
 	int  Window_Init(int width, int height, const char* const title);
 	void Window_SetIcon(int width, int height, unsigned char* pixels);
 	void Window_SetTitle(const char* const title);
+	void Window_SetBG(unsigned char r, unsigned char g, unsigned char b);
+	bool Window_ShouldClose();
+	void NewFrame();
+	void EndFrame();
 	void Window_Destroy();
 }
 
--- src/base/window.cpp
+++ src/base/window.cpp
@@ -4,7 +4,12 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
 static GLFWwindow* window = nullptr;
+static float window_bg[3] = { 0, 0, 0 };
 
 int ImBase::Window_Init(int width, int height, const char* const title) {
 	glfwInit();
@@ -34,6 +39,17 @@ int ImBase::Window_Init(int width, int height, const char* const title) {
 	}
 
 	glfwSwapInterval(0);
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 130");
+
+	ImGui::StyleColorsDark();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	io.IniFilename = NULL; // Disable Generation of .ini file
+
 	glfwShowWindow(window);
 
 	return 0;
@@ -51,7 +67,38 @@ void ImBase::Window_SetTitle(const char* const title) {
 	glfwSetWindowTitle(window, title);
 }
 
+void ImBase::Window_SetBG(unsigned char r, unsigned char g, unsigned char b) {
+	window_bg[0] = r / 255.0f;
+	window_bg[1] = g / 255.0f;
+	window_bg[2] = b / 255.0f;
+}
+
+bool ImBase::Window_ShouldClose() {
+	return glfwWindowShouldClose(window);
+}
+
+void ImBase::NewFrame() {
+	glfwPollEvents();
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplGlfw_NewFrame();
+	ImGui::NewFrame();
+}
+
+void ImBase::EndFrame() {
+	ImGui::Render();
+
+	glClearColor(window_bg[0], window_bg[1], window_bg[2], 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+	glfwSwapBuffers(window);
+}
+
 void ImBase::Window_Destroy() {
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	window = nullptr;
